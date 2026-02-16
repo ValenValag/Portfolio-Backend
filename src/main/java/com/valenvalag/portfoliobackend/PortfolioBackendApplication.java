bab0f1c9dcf6f4a99242a99c47594953ff1eb087
ValenValag <val.agarcia08@gmail.com>
2026-02-16 12:24:46 +0100

    Added / page.

--- src/main/java/com/valenvalag/portfoliobackend/PortfolioBackendApplication.java
+++ src/main/java/com/valenvalag/portfoliobackend/PortfolioBackendApplication.java
@@ -2,12 +2,18 @@ package com.valenvalag.portfoliobackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class PortfolioBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PortfolioBackendApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String mainPage(){
+        return "a";
     }
 
 }
